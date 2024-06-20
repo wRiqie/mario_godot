d080fa9ce8e41e5ce8aabfc32f44b2c413713ea4
wRiqie <henriquegame512@outlook.com>
2024-06-20 13:20:14 -0300

    Revert "fix: adjustments"
    This reverts commit f8f766564fec737de8f0ea25723755269ed9c849.

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -81,4 +81,4 @@ func _on_clear_trigger_area_entered(area):
 
 
 func _on_anim_animation_finished(anim_name):
-	SceneTransition.change_scene("res://Scenes/congratulations.tscn")
+	get_tree().change_scene_to_file("res://Scenes/congratulations.tscn")
